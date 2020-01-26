fdc1c92f8afb24dcc9db328743f67a6bf34b69f3
cx1964 <cx1964@mail.com>
2020-01-26 16:42:01 +0100

    stop and remove container

---
+++ Ubuntu1804/0090_stop_and_remove_container.sh
@@ -0,0 +1,10 @@
+# http://www.centinosystems.com/blog/sql/persisting-sql-server-data-in-docker-containers-part-1/
+
+# list docker containers
+docker ps -a
+
+# stop docker container
+docker stop <imagenumber>
+
+# remove docker container
+docker rm <imagenumber>
