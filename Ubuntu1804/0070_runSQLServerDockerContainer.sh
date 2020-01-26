82b1d55f0a84c6aa5bcf60b138c8efeb94f4f7f0
cx1964 <cx1964@mail.com>
2020-01-26 16:12:31 +0100

    cx1964@gmail.com

--- Ubuntu1804/0070_runSQLServerDockerContainer.sh
+++ Ubuntu1804/0070_runSQLServerDockerContainer.sh
@@ -14,3 +14,20 @@ docker run \
        -p 1433:1433 \
        -v sqldata1:/var/opt/mssql \
        -d mcr.microsoft.com/mssql/server:2019-GA-ubuntu-16.04
+
+
+# use DBeaver to test connection
+# https://dbeaver.io/    
+# use conection info:
+# SQL server
+#   host: localhost
+#   port: 1433
+#   Database/Schema: master   
+#   Autehnitaction: SQL Server Authentication
+#   username: sa
+#   pwassword: Welkom01
+#   Diver name: MS SQL Server/SQL Server
+#   Settings
+#           Thrust Server Certificate ON
+#
+# Use <test connection> button
