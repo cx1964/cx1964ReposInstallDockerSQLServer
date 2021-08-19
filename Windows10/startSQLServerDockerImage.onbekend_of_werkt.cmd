ce432292729099d6db38f57ae3cf8f6074d860c4
c.x. la fontaine <cx.la.fontaine@pzh.nl>
2021-08-19 17:14:42 +0200

    script en imaged edited

--- Windows10/startSQLServerDockerImage.onbekend_of_werkt.cmd
+++ Windows10/startSQLServerDockerImage.onbekend_of_werkt.cmd
@@ -1,4 +1,6 @@
 rem Functie: starten van een Docker image met SQL server en een Docker volumne in windows
+rem doc: https://hub.docker.com/_/microsoft-mssql-server
+
 set PASSWORD="Welkom01"
-docker run --name "sql19latest" --memory "1G" -e 'ACCEPT_EULA=Y' -e 'MSSQL_SA_PASSWORD='%PASSWORD% -p 1433:1433 -v mssqlserverdata1:/var/opt/mssql -d mcr.microsoft.com/mssql/server:latest
+docker run --name "sql19latest" --memory "1G" -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD='%PASSWORD% -p 1433:1433 -v mssqlserverdata1:/var/opt/mssql -d mcr.microsoft.com/mssql/server:2019-GA-ubuntu-16.04
 rem    -d mcr.microsoft.com/mssql/server:2019-GA-ubuntu-16.04
