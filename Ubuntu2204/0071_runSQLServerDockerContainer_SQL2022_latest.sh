e99ae035acde60738768f36088a4f1f86bfa28b3
cx1964 <cx1964@mail.com>
2023-11-26 15:47:50 +0100

    extra script + rename dir 22.04

---
+++ Ubuntu2204/0071_runSQLServerDockerContainer_SQL2022_latest.sh
@@ -0,0 +1,33 @@
+# http://www.centinosystems.com/blog/sql/persisting-sql-server-data-in-docker-containers-part-1/
+# remark: 
+
+# for avaiable SQL Server Docker containers see
+# https://hub.docker.com/_/microsoft-mssql-server
+
+
+export PASSWORD="Welkom01"
+# Run Docker conrainer met Docker volume
+# /var/opt/mssql
+docker run \
+       --name 'sql2022' \
+       -e 'ACCEPT_EULA=Y' -e 'MSSQL_SA_PASSWORD='$PASSWORD \
+       -p 1433:1433 \
+       -v sqldata1:/var/opt/mssql \
+       -d mcr.microsoft.com/mssql/server:2022-latest
+
+
+# use DBeaver to test connection
+# https://dbeaver.io/    
+# use conection info:
+# SQL server
+#   host: localhost
+#   port: 1433
+#   Database/Schema: master   
+#   Autehnitaction: SQL Server Authentication
+#   username: sa
+#   pwassword: Welkom01
+#   Diver name: MS SQL Server/SQL Server
+#   Settings
+#           Thrust Server Certificate ON
+#
+# Use <test connection> button
